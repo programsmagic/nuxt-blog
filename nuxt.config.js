94d846eda8ee9a6d8f557ecd21bbee3df7571cbb
Prashant Mishra <prashant.mishra@traderinteractive.com>
2022-02-26 13:20:31 +0530

    Google Analitics added

--- nuxt.config.js
+++ nuxt.config.js
@@ -84,7 +84,7 @@ export default {
   css: ['./assets/css/bootstrap.min.css', './assets/css/mediumish.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: ['@/plugins/vue-gtag'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -94,7 +94,8 @@ export default {
     // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss'
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/google-analytics'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
@@ -119,6 +120,48 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {}
-}
+  build: {},
 
+  googleAnalytics: {
+    id: 'G-K8FB55XTTC'
+  },
+
+  publicRuntimeConfig: {
+    googleAnalytics: {
+      id: 'G-K8FB55XTTC'
+    }
+  },
+
+  /*
+  ** Router configuration
+  */
+  router: {
+    scrollBehavior: async (to, from, savedPosition) => {
+      if (savedPosition) {
+        return savedPosition
+      }
+
+      const findEl = async (hash, x) => {
+        return document.querySelector(hash) ||
+          new Promise((resolve, reject) => {
+            if (x > 50) {
+              return resolve()
+            }
+            setTimeout(() => { resolve(findEl(hash, ++x || 1)) }, 100)
+          })
+      }
+
+      if (to.hash) {
+        const el = await findEl(to.hash)
+        if ('scrollBehavior' in document.documentElement.style) {
+          return window.scrollTo({ top: el.offsetTop, behavior: 'smooth' })
+        } else {
+          return window.scrollTo(0, el.offsetTop)
+        }
+      }
+
+      return { x: 0, y: 0 }
+    }
+  }
+
+}
---
+++ plugins/vue-gtag.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import VueAnalytics from 'vue-analytics'
+
+Vue.use(VueAnalytics, {
+  id: 'G-K8FB55XTTC',
+  autoTracking: {
+    screenview: true
+  }
+})
