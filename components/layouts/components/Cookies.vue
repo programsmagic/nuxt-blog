52fc3017e2f4d48d7a5d048a0b72cec078ea749a
Prashant Mishra <prashant.mishra@traderinteractive.com>
2022-03-05 04:01:10 +0530

    Added Some Extra Pages

--- components/layouts/components/Cookies.vue
+++ components/layouts/components/Cookies.vue
@@ -2,7 +2,7 @@
   <div v-if="isOpen" class="cookie">
     <div class="alertbar">
       <div class="container text-center">
-        <img src="https://programsmagic.com/img/logo.626b6e5d.png" alt=""> &nbsp; {{ message }}.
+        &nbsp; {{ message }}.
         &nbsp;
         <a class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full" @click="accept">{{ buttonTextAccept }}</a>&nbsp;
         <a class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-full" @click="deny">{{ buttonTextDeny }}</a>
